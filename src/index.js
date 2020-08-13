03cf8b2ac27c00f270e7e37012a2ce591e05fc20
MalgorzataAngular <malgorzata_szymanska_02@wp.pl>
2020-08-13 14:00:40 +0200

    Dedudant code not related with issue

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ import allReducers from './reducers';
 import { Provider } from 'react-redux';
 
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-fetch("https://randomuser.me/api/?results=10").then(response=>response.json()).then(response=>{
-
-})
 ReactDOM.render(
   <Provider store={store}>
     <App />
